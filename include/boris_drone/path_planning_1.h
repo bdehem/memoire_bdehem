6100f5f87967d38b6a95823391ce2cbc19ede503
bdehem <boris.deh@gmail.com>
2017-03-14 20:44:16 +0100

    commit de mardi

--- include/boris_drone/path_planning_1.h
+++ include/boris_drone/path_planning_1.h
@@ -49,66 +49,44 @@ private:
   std::string pose_channel;
   std::string strategy_channel;
 
-  int i;
-  int j;
   //! Callback when pose is received
   void poseCb(const boris_drone::Pose3D::ConstPtr posePtr);
   void strategyCb(const std_msgs::Int16::ConstPtr strategyPtr);
 
   int strategy;
 
-  float home_x;
-  float home_y;
-  float grid_origin_x;
-  float grid_origin_y;
+  double grid_origin_x;
+  double grid_origin_y;
 
   int myGrid[SIDE * 10][SIDE * 10];
   int gridSize;
   int nExploredCell;
 
+  boris_drone::Pose3D pose;
+
+  bool gridInitialized;
+
+  double XMax;
+  double YMax;
 public:
   //! Constructor
   PathPlanning();
   //! Destructor
   ~PathPlanning();
-
-
-  boris_drone::Pose3D pose;
-
-  boris_drone::cellUpdate cellUpdateMsg;
-  bool gridInitialized;
-  void init();
-  double next_x;
-  double next_y;
-  double next_z;
-  double next_rotZ;
-  bool instruction_publishing;
-  void publish_poseref(bool takeoff, bool land);
-  void yaw_desired();
-  bool xy_desired();
+  void publish_poseref(double x_ref, double y_ref, double z_ref, double rotZ_ref, bool takeoff, bool land);
   void reset();
-  void SetRef(double x_ref, double y_ref, double z_ref, double rotZ_ref);
   void InitializeGrid();
-  int bordersList[SIDE * 100];  // What is the maximum number of frontier cells?
-  bool ThereIsAWallCell(int i, int j);
   void UpdateMap(double x, double y);
   void GetFieldOfView(double x, double y, int* iMin, int* iMax, int* jMin, int* jMax);
-  void advanced_xy_desired();
+  int inGrid(int i);
+  void advanced_xy_desired(double* x, double* y);
   void CellToXY(int i, int j, double* xfromcell, double* yfromcell);
-  int* XYToCell(double x, double y);
-  double sqdistance(int i, int j, int k, int l);
+  void XYToCell(double x, double y, int* i, int* j);
+  int sqdistance(int i, int j, int k, int l);
   int getStrategy();
-  double XMax;
-  double YMax;
-  int CellUp;
-  int CellDown;
-  int CellRight;
-  int CellLeft;
-  double xfromcell;
-  double yfromcell;
-  double xfromcell2;
-  double yfromcell2;
-  double alt;
+  void seek();
+  void explore();
+  void backToBase();
 };
 
 #endif /*boris_drone_PATH_PLANNING_H */
--- src/path_planning/path_planning_1.cpp
+++ src/path_planning/path_planning_1.cpp
@@ -42,7 +42,6 @@ PathPlanning::PathPlanning()
   explore_channel = nh.resolveName("finished_exploring");
   explore_pub = nh.advertise<std_msgs::Empty>(explore_channel, 1);
 
-  // instruction_publishing = false;
 }
 
 // Destructor
@@ -54,35 +53,29 @@ PathPlanning::~PathPlanning()
 // Function to reset the desired pose sent to the controller
 void PathPlanning::reset()
 {
-  next_x = 0;
-  next_y = 0;
-  next_z = 1;
-  next_rotZ = 0;
-  i = 0;
+  double next_z = 1.0;
   gridInitialized = false;
 
   // XMax and YMax are the half of the distance between the horizontal and vertical border lines of
   // the vision of the drone. Those values come from bottom camera data and 0.8 is used as a
   // security factor in the case of the drone is not well oriented.
-
   XMax = next_z * 0.435 * 0.8 * 0.5;
   YMax = next_z * 0.326 * 0.8 * 0.5;
 }
 
 // This function publish the position where the drone has to get. It is used as pose_ref in the
 // controller node.
-void PathPlanning::publish_poseref(bool takeoff, bool land)
+void PathPlanning::publish_poseref(double x_ref, double y_ref, double z_ref, double rotZ_ref, bool takeoff, bool land)
 {
   // instantiate the poseref message
   boris_drone::PoseRef poseref_msg;
 
-  poseref_msg.x = next_x;
-  poseref_msg.y = next_y;
-  poseref_msg.z = next_z;
-  poseref_msg.rotZ = next_rotZ;
+  poseref_msg.x = x_ref;
+  poseref_msg.y = y_ref;
+  poseref_msg.z = z_ref;
+  poseref_msg.rotZ = rotZ_ref;
   poseref_msg.landAndStop = land;
   poseref_msg.takeoffAndStart = takeoff;
-
 
   poseref_pub.publish(poseref_msg);
 }
@@ -108,36 +101,6 @@ int PathPlanning::getStrategy()
   return strategy;
 }
 
-// First approach to explore a map. The drones makes a kind of labyrinth
-
-bool PathPlanning::xy_desired()
-{
-  // The drone is considered as "arrived on poseref if it in a radius of 0.35m"
-  if (sqrt((pose.x - next_x) * (pose.x - next_x) +
-           (pose.y - next_y) * (pose.y - next_y)) < 0.35)
-  {
-    printf("i: %d\n", i);
-    if (i % 2 == 0)
-    {
-      next_x += 1;
-    }
-    else if (i % 4 == 1)
-    {
-      next_y += 1;
-    }
-    else  //(i%4==3)
-    {
-      next_y -= 1;
-    }
-    i++;
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
-
 // In our final approach to expore the map, the pathplanning creates a grid. This grid is made of
 // 0.10*0.10 m² cells that can be in 3 different states :
 // 0 means unseen/unexplored
@@ -150,9 +113,9 @@ void PathPlanning::InitializeGrid()
   // Set all the cells to "unexplored"
   grid_origin_x = pose.x - SIDE/2.0;
   grid_origin_y = pose.y - SIDE/2.0;
-  for (i = 0; i < SIDE * 10; i++)
+  for (int i = 0; i < SIDE * 10; i++)
   {
-    for (j = 0; j < SIDE * 10; j++)
+    for (int j = 0; j < SIDE * 10; j++)
     {
       myGrid[i][j] = 0;
     }
@@ -168,51 +131,70 @@ void PathPlanning::InitializeGrid()
 // !! abscisse = I and ordoonee = J
 void PathPlanning::GetFieldOfView(double x, double y, int* iMin, int* iMax, int* jMin, int* jMax)
 {
-
-  int* ijMax = XYToCell(x+XMax,y+YMax);
-  int* ijMin = XYToCell(x-XMax,y-YMax);
-  *iMax = ijMax[0];
-  *jMax = ijMax[1];
-  *iMin = ijMin[0];
-  *jMin = ijMin[1];
+  ROS_INFO("======================================Getting FOV================================================");
+  XYToCell(x+XMax,y+YMax,iMax,jMax);
+  XYToCell(x-XMax,y-YMax,iMin,jMin);
+  ROS_INFO("========================================Got FOV==================================================");
 }
 
 // This function transforms i and j coordinates to x and y position in the real playground.
 void PathPlanning::CellToXY(int i, int j, double* x, double* y)
 {
-  ROS_INFO("test: start celltoxy: %f, %f, %d, %d",*x,*y,i,j);
   *x = grid_origin_x + (double)(i * (SIDE/10.0));
   *y = grid_origin_y + (double)(j * (SIDE/10.0));
 }
 
-int* PathPlanning::XYToCell(double x, double y)
+//void PathPlanning::XYToCell(double x, double y, int* i, int* j)
+//{
+//  ROS_INFO("start XYToCell:x,y = %f, %f",x,y);
+//  *i = (int)((x - grid_origin_x) / (SIDE/10.0));
+//  *j = (int)((y - grid_origin_y) / (SIDE/10.0));
+//  ROS_INFO("end XYToCell:i,j = %d, %d",*i,*j);
+//}
+
+void PathPlanning::XYToCell(double x, double y, int* i, int* j)
 {
-  ROS_INFO("start XYToCell: %f, %f",x,y);
-  ROS_INFO("grid origin: %f, %f",grid_origin_x,grid_origin_y);
-  int i = (int)((x - grid_origin_x) / (SIDE/10.0));
-  int j = (int)((y - grid_origin_y) / (SIDE/10.0));
-  ROS_INFO("mid XYToCell: %f, %f",x,y);
-  int ij[2] = {i, j};
-  return ij;
+  ROS_INFO("start XYToCell:x,y = %f, %f",x,y);
+  double xrel = x - grid_origin_x;
+  double yrel = y - grid_origin_y;
+  ROS_INFO("xrel,yrel = %f, %f",xrel,yrel);
+  double di = xrel / (SIDE/10.0);
+  double dj = yrel / (SIDE/10.0);
+  ROS_INFO("di,dj = %f, %f",di,dj);
+  int ii = di;
+  int jj = dj;
+  ROS_INFO("ii,jj = %d, %d",ii,jj);
+  int* pi = &ii;
+  int* pj = &jj;
+  ROS_INFO("pi,pj = %p, %p",pi,pj);
+  ROS_INFO("*pi,*pj = %d, %d",*pi,*pj);
+  i = pi;
+  j = pj;
+  ROS_INFO("end XYToCell:i,j = %d, %d",*i,*j);
 }
 
+int PathPlanning::inGrid(int i)
+{
+  return std::max(0,std::min(10*SIDE-1,i));
+}
 
 void PathPlanning::UpdateMap(double x, double y)
 {
-  ROS_INFO_THROTTLE(3,"I'm in the updatemap function");
+  if (!gridInitialized)
+  {
+    InitializeGrid();
+  }
   int iMin;
   int jMin;
   int iMax;
   int jMax;
   GetFieldOfView(x, y, &iMin, &iMax, &jMin, &jMax);
-  boris_drone::cellUpdate cellUpdateMsg;
-  for (i = iMin; i < iMax; i++)
+  for (int i = inGrid(iMin); i < inGrid(iMax); i++)
   {
-    for (j = jMin; j < jMax; j++)
+    for (int j = inGrid(jMin); j < inGrid(jMax); j++)
     {
       if (myGrid[i][j] != EXPLORED)
       {
-        nExploredCell++;
         if (i == iMin || i == iMax - 1 || j == jMin || j == jMax - 1)
         {
           myGrid[i][j] = BORDER;
@@ -220,15 +202,17 @@ void PathPlanning::UpdateMap(double x, double y)
         else
         {
           myGrid[i][j] = EXPLORED;
+          nExploredCell++;
         }
       }
     }
   }
   ROS_INFO_THROTTLE(3,"Map updated. Explored %d/%d cells",nExploredCell,gridSize);
+  ROS_INFO_THROTTLE(3,"current FOV: i=%d-%d; j=%d-%d",iMin,iMax,jMin,jMax);
 }
 
 // This function returns the squared distance between two cells.
-double PathPlanning::sqdistance(int i, int j, int k, int l)
+int PathPlanning::sqdistance(int i, int j, int k, int l)
 {
   return (i - k)*(i - k) + (j - l)*(j - l);
 }
@@ -236,113 +220,101 @@ double PathPlanning::sqdistance(int i, int j, int k, int l)
 // This function replaced the labyrtinth one. It takes as argument the position of the drone and
 // gives back the best cell where the drone must go. In order to do that, it compares all the border
 // cells of the map and tell the drone to go to the nearrest one.
-void PathPlanning::advanced_xy_desired()
+void PathPlanning::advanced_xy_desired(double* x, double* y)
 {
-  ROS_INFO("start advanced_xy_desired");
-  double k;
-  double l;
-  int* ij = XYToCell(pose.x,pose.y);
-  int drone_i = ij[0];
-  int drone_j = ij[1];
-  double bestDist = 1000000.0;
-  double currDist;
+//  ROS_INFO("start advanced_xy_desired");
+//  int* drone_i;
+//  int* drone_j;
+//  ROS_INFO("=====================================Getting IJPOS ===============================================");
+//  XYToCell(pose.x,pose.y,drone_i,drone_j);
+//  ROS_INFO("=======================================Got IJPOS =================================================");
+  int bestDist = 1;
+  int currDist;
   int closestJ = 0;
   int closestI = 0;
-  ROS_INFO("before for");
-  for (i = 0; i < SIDE * 10; i++)
-  {
-    for (j = 0; j < SIDE * 10; j++)
-    {
-      currDist = sqdistance(drone_i, drone_j, i, j);
-      if (myGrid[i][j] == BORDER &&  currDist < bestDist)
-      {
-        bestDist = currDist;
-        closestJ = j;
-        closestI = i;
-      }
-    }
-  }
-  ROS_INFO("before if");
-  if (bestDist == 1000000.0)
+
+//  for (int i = 0; i < SIDE * 10; i++)
+//  {
+//    for (int j = 0; j < SIDE * 10; j++)
+//    {
+//      currDist = sqdistance(*drone_i, *drone_j, i, j);
+//      if (myGrid[i][j] == BORDER &&  currDist < bestDist)
+//      {
+//        bestDist = currDist;
+//        closestJ = j;
+//        closestI = i;
+//      }
+//    }
+//  }
+  if (bestDist == 1000)
   {
     std_msgs::Empty empty_msg;
     explore_pub.publish(empty_msg);
     ROS_INFO("The map was explored completely, there are no more border cells!");
   }
-  ROS_INFO("test: beforecelltoxy");
-  CellToXY(closestI, closestJ, &k, &l);
-  SetRef(k,l,next_z,0);
+  CellToXY(closestI, closestJ, x, y);
 }
 
-// This function give to the object variable the computed next reference position.
-void PathPlanning::SetRef(double x_ref, double y_ref, double z_ref, double rotZ_ref)
+void PathPlanning::seek()
 {
-  this->next_x = x_ref;
-  this->next_y = y_ref;
-  this->next_z = z_ref;
-  this->next_rotZ = rotZ_ref;
+  publish_poseref(pose.x+1.0, 0,1,0,false,false);
 }
 
-// Main function, will publish pose_ref with regard to the selected strategy comming from the
+void PathPlanning::explore()
+{
+  UpdateMap(pose.x, pose.y);
+  double* x;
+  double* y;
+  advanced_xy_desired(x,y);
+  publish_poseref(*x,*y,1,0,false,false);
+}
+
+void PathPlanning::backToBase()
+{
+  publish_poseref(0,0,1,0,false,false);
+}
+
+
+
+
+// Main function, will publish pose_ref with regard to the selected strategy coming from the
 // Strategy node.
 int main(int argc, char** argv)
 {
-  double *poseRefX, *poseRefY;
   ros::init(argc, argv, "path_planning");
   PathPlanning myPath;
-  ros::Rate r(20);  // Refresh every 1/20 second.
-  int strategy = myPath.getStrategy();
-  ROS_DEBUG("path planning initialized");
-
   myPath.reset();
-  myPath.publish_poseref(false,false);
+  ros::Rate r(20);  // Refresh every 1/20 second.
+
+  myPath.publish_poseref(0,0,1,0,false,false);
   ros::spinOnce();
   r.sleep();
   ROS_INFO("poseref initialized and launched");
   while (ros::ok())
   {
     TIC(path);
-    strategy = myPath.getStrategy();
-    if (strategy == WAIT)//0
+    switch(myPath.getStrategy())
     {
-      myPath.publish_poseref(false,false);
+      case WAIT:
+        myPath.publish_poseref(0,0,1,0,false,false);
+        break;
+      case TAKEOFF:
+        myPath.publish_poseref(0,0,1,0,true,false);
+        break;
+      case SEEK:
+        myPath.seek();
+        break;
+      case EXPLORE:
+        myPath.explore();
+        break;
+      case BACKTOBASE:
+        myPath.backToBase();
+        break;
+      case LAND:
+        myPath.publish_poseref(0,0,0,0,false,true);
+        ROS_INFO("landing");
+        break;
     }
-    else if (strategy == TAKEOFF)//1
-    {
-      myPath.publish_poseref(true,false);
-    }
-    else if (strategy == SEEK)//2
-    {
-      myPath.SetRef(myPath.pose.x + 1.0, 0, myPath.next_z, 0);
-      myPath.publish_poseref(false,false);
-    }
-    else if (strategy == EXPLORE)//3
-    {
-      if (!myPath.gridInitialized)
-      {
-        ROS_INFO("I am initializing the grid");
-        myPath.InitializeGrid();
-      }
-      myPath.UpdateMap(myPath.pose.x, myPath.pose.y);
-      ROS_INFO("before advancexydesired");
-      myPath.advanced_xy_desired();
-      ROS_INFO("poseref is : (%lf, %lf)", *poseRefX, *poseRefY);
-
-      ros::Duration(1).sleep();
-      myPath.publish_poseref(false,false);
-    }
-    else if (strategy == BACKTOBASE)
-    {
-      myPath.SetRef(0,0,myPath.next_z,myPath.pose.rotZ);
-      myPath.publish_poseref(false,false);
-    }
-    else if (myPath.getStrategy() == LAND)
-    {
-      ROS_INFO("landing");
-      myPath.SetRef(0,0,0,0);
-      myPath.publish_poseref(false,true);
-    }
-
 
     //TOC(path, "path planning");
     ros::spinOnce();
